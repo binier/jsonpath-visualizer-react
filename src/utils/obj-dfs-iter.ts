e56cc92298c6ddba0735f0c374c85ca7256c8381
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-20 12:59:15 +0400

    feat: implement Object depth-first search

---
+++ src/utils/obj-dfs-iter.ts
@@ -0,0 +1,52 @@
+export interface ObjDfsIterValue {
+  key: string;
+  value: any;
+}
+
+export interface ObjDfsIter {
+  next(): ObjDfsIterValue | Symbol;
+  nextAll(): ObjDfsIterValue[];
+}
+
+const ITER_END = Symbol('Iterator End');
+
+/**
+ * Object depth-first search (dfs preordered)
+ *
+ * First visited edges are returned first.
+ */
+export function objDfsIter(obj: Record<string, any>): ObjDfsIter {
+  const keys = Object.keys(obj);
+  let index = -1;
+  let nestedIter: ObjDfsIter;
+
+  return {
+    next() {
+      if (nestedIter) {
+        const nestedNext = nestedIter.next();
+        if (nestedNext !== ITER_END)
+          return nestedNext;
+      }
+      ++index;
+
+      if (index === keys.length)
+        return ITER_END;
+
+      const key = keys[index],
+            value = obj[key];
+
+      if (typeof value === 'object')
+        nestedIter = objDfsIter(value);
+
+      return { key, value };
+    },
+    nextAll() {
+      const result: ObjDfsIterValue[] = [];
+      let last;
+
+      while((last = this.next()) !== ITER_END)
+        result.push(last as ObjDfsIterValue);
+      return result;
+    },
+  };
+}
