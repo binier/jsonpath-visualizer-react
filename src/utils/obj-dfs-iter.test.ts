572c9a3f0777e6db15d23ed5bcb57a7b5d5013e7
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-20 13:03:20 +0400

    test: add simple test for Object depth-first search

---
+++ src/utils/obj-dfs-iter.test.ts
@@ -0,0 +1,30 @@
+import { objDfsIter } from './obj-dfs-iter';
+
+describe('Object DFS Iterator', () => {
+  it('next() outputs in correct order', () => {
+    const iter = objDfsIter({
+      users: [
+        { name: 'user1', age: 22 },
+        { name: 'user2', age: 23 },
+      ],
+      balls: ['ball1', 'ball2'],
+    });
+
+    const res = iter.nextAll();
+    expect(res.length).toEqual(10);
+    expect(res[0].key).toEqual('users');
+
+    expect(res[1].key).toEqual('0');
+    expect(res[2]).toMatchObject({ key: 'name', value: 'user1' });
+    expect(res[3]).toMatchObject({ key: 'age', value: 22 });
+
+    expect(res[4].key).toEqual('1');
+    expect(res[5]).toMatchObject({ key: 'name', value: 'user2' });
+    expect(res[6]).toMatchObject({ key: 'age', value: 23 });
+
+    expect(res[7].key).toEqual('balls');
+
+    expect(res[8]).toMatchObject({ key: '0', value: 'ball1' });
+    expect(res[9]).toMatchObject({ key: '1', value: 'ball2' });
+  });
+});
