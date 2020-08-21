74ef5dcfaada474346f778bbdec4891d6de43c99
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-21 15:09:03 +0400

    feat(obj-dfs-iter): add reverse iteration feature
    BREAKING CHANGE: switch from function to class.

--- src/utils/obj-dfs-iter.ts
+++ src/utils/obj-dfs-iter.ts
@@ -1,52 +1,124 @@
 export interface ObjDfsIterValue {
   key: string;
   value: any;
+  depth: number;
 }
 
-export interface ObjDfsIter {
-  next(): ObjDfsIterValue | Symbol;
-  nextAll(): ObjDfsIterValue[];
-}
-
-const ITER_END = Symbol('Iterator End');
+export const ITER_END = Symbol('Iterator End');
 
 /**
  * Object depth-first search (dfs preordered)
  *
  * First visited edges are returned first.
  */
-export function objDfsIter(obj: Record<string, any>): ObjDfsIter {
-  const keys = Object.keys(obj);
-  let index = -1;
-  let nestedIter: ObjDfsIter;
+export class ObjDfsIter {
+  private index: number;
+  private depth: number;
+  private keys: string[];
+  private nestedIter?: ObjDfsIter;
 
-  return {
-    next() {
-      if (nestedIter) {
-        const nestedNext = nestedIter.next();
-        if (nestedNext !== ITER_END)
-          return nestedNext;
-      }
-      ++index;
+  constructor(
+    private obj: Record<string, any>,
+    { depth, index } = { depth: 0, index: -1 }
+  ) {
+    this.depth = depth;
+    this.index = index;
+    this.keys = Object.keys(obj);
+  }
 
-      if (index === keys.length)
-        return ITER_END;
+  private getKeyValue(index = this.index) {
+    const key = this.keys[index];
+    return { key, value: this.obj[key] };
+  }
 
-      const key = keys[index],
-            value = obj[key];
+  next(): ObjDfsIterValue | Symbol {
+    if (this.nestedIter) {
+      const nestedNext = this.nestedIter.next();
+      if (nestedNext !== ITER_END)
+        return nestedNext;
+      this.nestedIter = undefined;
+    }
 
-      if (typeof value === 'object' && value)
-        nestedIter = objDfsIter(value);
+    if (++this.index === this.keys.length)
+      return ITER_END;
 
-      return { key, value };
-    },
-    nextAll() {
-      const result: ObjDfsIterValue[] = [];
-      let last;
+    const {key, value} = this.getKeyValue(this.index);
 
-      while((last = this.next()) !== ITER_END)
-        result.push(last as ObjDfsIterValue);
-      return result;
-    },
-  };
+    if (typeof value === 'object' && value)
+      this.nestedIter = new ObjDfsIter(
+        value,
+        { index: -1, depth: this.depth + 1 }
+      );
+
+    return { key, value, depth: this.depth };
+  }
+
+  prev(): ObjDfsIterValue | Symbol {
+    if (this.nestedIter) {
+      const nestedPrev = this.nestedIter.prev();
+      if (nestedPrev !== ITER_END)
+        return nestedPrev;
+      this.nestedIter = undefined;
+      return { ...this.getKeyValue(this.index), depth: this.depth };
+    }
+    if (--this.index < 0)
+      return ITER_END;
+
+    const {key, value} = this.getKeyValue(this.index);
+    if (typeof value === 'object' && value) {
+      this.nestedIter = new ObjDfsIter(
+          value,
+          { index: -1, depth: this.depth + 1 }
+        )
+        .nextSkipAll();
+      return this.prev();
+    }
+    return { key, value, depth: this.depth };
+  }
+
+  nextSkip(count: number) {
+    for (let i = 0; i < count; ++i, this.next());
+    return this;
+  }
+
+  nextSkipAll() {
+    this.index = this.keys.length;
+    return this;
+  }
+
+  private collectAll(
+    direction: 'next' | 'prev' = 'next',
+    count = Infinity
+  ) {
+    const next = direction === 'next'
+      ? () => this.next()
+      : () => this.prev();
+
+    const result: ObjDfsIterValue[] = [];
+
+    for (let i = 0, cur: ObjDfsIterValue | Symbol; i < count; ++i) {
+      cur = next();
+      if (cur === ITER_END) break;
+      result.push(cur as ObjDfsIterValue);
+    }
+
+    return result;
+  }
+
+  nextAll(count: number = Infinity) {
+    return this.collectAll('next', count);
+  }
+
+  prevAll(count: number = Infinity) {
+    return this.collectAll('prev', count);
+  }
+
+  /** **Warning:** consumes iterator! */
+  count() {
+    let count = 0;
+
+    while (this.next() !== ITER_END)
+      ++count;
+    return count;
+  }
 }
