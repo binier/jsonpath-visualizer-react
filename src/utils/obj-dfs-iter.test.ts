3169ae6652d61b2440cfee0230abbf1888943236
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-21 15:11:01 +0400

    test(obj-dfs-iter): reverse iteration

--- src/utils/obj-dfs-iter.test.ts
+++ src/utils/obj-dfs-iter.test.ts
@@ -41,4 +41,27 @@ describe('Object DFS Iterator', () => {
     expect(res[8]).toMatchObject({ key: '0', value: 'ball1' });
     expect(res[9]).toMatchObject({ key: '1', value: 'ball2' });
   });
+
+  it('prev() outputs in correct order', () => {
+    const iter = new ObjDfsIter({
+      users: [
+        { name: 'user1', age: 22 },
+        { name: 'user2', age: 23 },
+      ],
+      balls: ['ball1', 'ball2'],
+    }).nextSkipAll();
+
+    const res = iter.prevAll();
+    expect(res.length).toEqual(10);
+    expect(res[0]).toMatchObject({ key: '1', value: 'ball2' });
+    expect(res[1]).toMatchObject({ key: '0', value: 'ball1' });
+    expect(res[2].key).toEqual('balls');
+    expect(res[3]).toMatchObject({ key: 'age', value: 23 });
+    expect(res[4]).toMatchObject({ key: 'name', value: 'user2' });
+    expect(res[5].key).toEqual('1');
+    expect(res[6]).toMatchObject({ key: 'age', value: 22 });
+    expect(res[7]).toMatchObject({ key: 'name', value: 'user1' });
+    expect(res[8].key).toEqual('0');
+    expect(res[9].key).toEqual('users');
+  });
 });
