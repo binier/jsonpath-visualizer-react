884912a42155ceb45f4118abc1ab8f363d147d06
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-24 13:32:26 +0400

    feat: display sample data in JsonView

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { JsonView } from '../json-view';
+
+function sampleJson(count: number) {
+  const rAge = () => Math.floor(Math.random() * 100);
+  const nextUser = (() => {
+    let i = 0;
+    return () => ({ name: 'user' + ++i, age: rAge() });
+  })();
+  return {
+    result: {
+      users: [...Array(count)].map(_ => nextUser()),
+    },
+  };
+}
 
 export default function () {
-  return (<div></div>);
+  const json = sampleJson(1000);
+  return (
+    <div className="w-full p-2">
+      <div className="text-center">
+        <JsonView json={json}></JsonView>
+      </div>
+    </div>
+  );
 };
