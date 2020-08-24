7b3f4823c565fbd6801127f1080d465964514231
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-24 08:13:25 +0400

    test: json to `ElementData[]` converter

---
+++ src/components/json-view/json-to-elements.test.ts
@@ -0,0 +1,18 @@
+import { jsonToElements } from './json-to-elemets';
+
+const buildElements = () => jsonToElements({
+  users: [
+    { name: 'user1', age: '100', notes: ['abc', 'def'] },
+    { name: 'user2', age: '200', notes: ['ghi', 'jkl'] },
+  ],
+});
+
+describe('json to elemets test', () => {
+  it('calculates childrenCount correctly', () => {
+    const elements = buildElements();
+    expect(elements[0].childrenCount).toEqual(17);
+    expect(elements[1].childrenCount).toEqual(7);
+    expect(elements[2].childrenCount).toEqual(0);
+    expect(elements[4].childrenCount).toEqual(3);
+  });
+});
