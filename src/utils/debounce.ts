add6ec727ecc4ea7badcf2aba7484f8fd6f40701
Zura Benashvili <zura.benashvili@gmail.com>
2020-08-24 08:08:54 +0400

    feat(utils): add debounce

---
+++ src/utils/debounce.ts
@@ -0,0 +1,10 @@
+export function debounce(fn: Function, ms: number) {
+  let timer: any;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn();
+    }, ms);
+  };
+}
